e6c81a53dfad4a98129945c75276b968fdd75d5d
Arvid Pilhall <arvidp@axis.com>
2019-07-30 15:21:30 +0200

    Login page started
    Change-Id: I1216c74349e6dea3994465f7b2f22b383de3b0d8

--- fridge/src/App.js
+++ fridge/src/App.js
@@ -1,58 +1,115 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
+const styles = {
+  textField: {
+    margin: 10,
+  },
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    margin: 10,
+  },
+}
+
+const { button, textField } = styles
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
-      items: []
+      isLoading: false,
+      items: [],
+      email: null,
+      password: null
     }
     this.connecToServer = this.connecToServer.bind(this);
   }
 
   connecToServer() {
-    console.log("oiajsdoaijsd")
-    fetch('/api/grocery')
-      .then(res => res.json())
-      .then(result => {
-          console.log(result)
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+
   }
 
   componentDidMount() {
     this.connecToServer();
   }
 
-   render() {
-     const { error, isLoaded, items } = this.state;
-     return (
-        <div className="container">
-          {items.map((item) => (
-            <li key={item.id}>
-              {item.name}
-            </li>
-          ))}
-          <Button variant="contained" color="secondary">
-            Default
-          </Button>
-        </div>
+  async login() {
+    fetch('/api/login/' + this.state.email + "/" + this.state.password)
+      .then(res => res.json())
+      .then(result => {
+          console.log(result.message)
+          this.setState({
+            isLoading: false,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoading: false,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    var CryptoJS = require("crypto-js");
+
+    // Encrypt
+    var ciphertext = CryptoJS.AES.encrypt('123456789!', 'secret key 123');
+    var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
+    var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+
+    const { error, isLoaded, items } = this.state;
+    return (
+      <div className="container">
+        <Grid container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              style={{ minHeight: '100vh' }}
+              >
+          <Grid item xs={6}>
+            <TextField
+              id="outlined-email-input"
+              label="Email"
+              type="email"
+              name="email"
+              autoComplete="email"
+              margin="normal"
+              variant="outlined"
+              style={textField}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              variant="outlined"
+              style={textField}
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </Grid>
+            <Button variant="contained" color="primary" style={button} onClick={() => { this.login() }}>
+              Login
+            </Button>
+          </Grid>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -17,37 +17,91 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 db.serialize(function() {
-    db.run(`CREATE TABLE IF NOT EXISTS fridge (
-            id        integer,
-            owner     string,
+    db.run("DROP TABLE IF EXISTS users")
+    db.run("DROP TABLE IF EXISTS fridges")
+    db.run("DROP TABLE IF EXISTS groceries")
+    db.run(`CREATE TABLE users (
+            name      TEXT,
+            email     TEXT,
+            password  TEXT,
+
+            fridgeID  INT NOT NULL,
+
+            PRIMARY KEY (email),
+            FOREIGN KEY (fridgeID) REFERENCES fridges(id)
+    )`)
+    db.run(`CREATE TABLE fridges (
+            id        INT,
+            size      INT,
 
             PRIMARY KEY (id)
     )`)
-    db.run(`CREATE TABLE IF NOT EXISTS grocery (
-            id        integer,
-            name      string,
-            weight    int,
-            category  string,
-            fridgeID,
+    db.run(`CREATE TABLE groceries (
+            id        INT,
+            name      TEXT,
+            weight    INT,
+            category  TEXT,
+
+            fridgeID  INT NOT NULL,
 
             PRIMARY KEY (id)
-            FOREIGN KEY (fridgeID) REFERENCES Fridge(id)
+            FOREIGN KEY (fridgeID) REFERENCES fridges(id)
     )`)
-    db.run(`INSERT INTO fridge (owner) VALUES ("Arvid")`)
-    db.run(`INSERT INTO grocery (name, weight, category) VALUES ("Milk", 1, "Dairy")`)
-    db.run(`INSERT INTO grocery (name, weight, category) VALUES ("Milk2", 1, "Dairy")`)
+    db.run(`INSERT INTO fridges (size) VALUES (20)`)
+    db.run(`INSERT INTO users (name, email, password, fridgeID) VALUES ("admin@admin.com", "arvid@admin.com", "135711Ap!", 1)`)
+    db.run(`INSERT INTO users (name, email, password, fridgeID) VALUES ("Siri", "siri@admin.com", "hejhej", 1)`)
+    db.run(`INSERT INTO groceries (name, weight, category, fridgeID) VALUES ("Milk", 1, "Dairy", 1)`)
+    db.run(`INSERT INTO groceries (name, weight, category, fridgeID) VALUES ("Milk2", 1, "Dairy", 1)`)
 });
 
 app.get('/api/fridge', (req, res) => {
-  db.get("SELECT * FROM fridge", function(err, row){
-        res.json(row);
-    });
+  db.get("SELECT * FROM fridges", function(err, row){
+    if (err) {
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.json({
+        "message":"success",
+        "data":row
+    })
+  });
 });
 
 app.get('/api/grocery', (req, res) => {
-  db.all("SELECT * FROM grocery WHERE category=\"Dairy\"", function(err, row){
-        res.json(row);
-    });
+  db.all("SELECT * FROM groceries", function(err, row){
+    if (err) {
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.json({
+        "message":"success",
+        "data":row
+    })
+  });
+});
+
+app.get("/api/login/:username/:password", (req, res, next) => {
+    var sql = "select password FROM users WHERE name=?"
+    var params = [req.params.username]
+    db.get(sql, params, (err, row) => {
+        console.log(params)
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        if (req.params.password === row.password) {
+          res.json({
+              "message":"success",
+              "data": null
+          })
+        } else {
+          res.json({
+              "message":"wrong password",
+              "data": null
+          })
+          return;
+        }
+      });
 });
 
 //build mode
@@ -59,13 +113,3 @@ app.get('*', (req, res) => {
 app.listen(port, (req, res) => {
   console.log( `server listening on port: ${port}`);
 })
-//
-// //Route setup
-// app.get('/', (req, res) => {
-//   res.send('rootadasd route');
-// })
-//
-// //Start server
-// app.listen(port, (req, res) => {
-//   console.log(`server listening on port: ${port}`)
-// });
