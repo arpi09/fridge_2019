f7775338378304a02a60a92c506e7c664211c148
Arvid <arvid.pilhall@me.com>
2019-11-03 13:34:10 +0100

    Fixed config bug

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 let jwt = require('jsonwebtoken');
-const config = require('./config');
+// const config = require('./config');
 
 let checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
